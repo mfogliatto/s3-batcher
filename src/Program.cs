c4d54805d54e0a44a04abd626cf30efc3251437a
Marco Fogliatto <MarcoFo@moravia.com>
2018-04-29 09:01:17 +0200

    Rename project name

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace s3_batcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+    }
+}
